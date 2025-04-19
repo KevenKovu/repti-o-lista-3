7b3a77b510d7a210c1aa37bbaf3b448ffbf431a0
Keven Moreno Araújo <kevenmorenoaraujo@gmail.com>
2025-04-19 20:45:52 +0000

    penultima alteração

---
+++ at6.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class at6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        List<String> nomesPacientes = new ArrayList<>();
+        List<String> cpfs = new ArrayList<>();
+        List<Integer> idades = new ArrayList<>();
+        List<String> diagnosticos = new ArrayList<>();
+
+        System.out.println("Quantos pacientes deseja cadastrar?");
+        int total = input.nextInt();
+        input.nextLine(); // Consumir a quebra de linha
+
+        // Cadastro
+        for (int i = 0; i < total; i++) {
+            System.out.println("Digite o nome:");
+            String nome = input.nextLine();
+            nomesPacientes.add(nome);
+
+            System.out.println("Digite o CPF:");
+            String cpf = input.nextLine();
+            cpfs.add(cpf);
+
+            System.out.println("Digite a idade:");
+            int idade = input.nextInt();
+            input.nextLine(); // Consumir a quebra de linha
+            idades.add(idade);
+
+            System.out.println("Digite o diagnóstico do paciente:");
+            String diagnostico = input.nextLine();
+            diagnosticos.add(diagnostico);
+            System.out.println("Paciente cadastrado com sucesso!\n");
+        }
+    }
+}
